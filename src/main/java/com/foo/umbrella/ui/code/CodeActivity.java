f74cfd48322420e8d7f074838aee19d64a747e98
clovis <clovajob@yahoo.com>
2017-10-04 23:57:52 -0400

    Pass city to Activity2

--- src/main/java/com/foo/umbrella/ui/code/CodeActivity.java
+++ src/main/java/com/foo/umbrella/ui/code/CodeActivity.java
@@ -1,15 +1,27 @@
 package com.foo.umbrella.ui.code;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.foo.umbrella.R;
 
 
 public class CodeActivity extends AppCompatActivity {
+    Intent secondIntent;
+    String city;
+
+    TextView secView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code);
+        city = secondIntent.getExtras().get("zip").toString();
+
+        secView = (TextView) findViewById(R.id.sec_tvId);
+        secView.setText(city);
+
     }
 }
--- src/main/java/com/foo/umbrella/ui/main/MainActivity.java
+++ src/main/java/com/foo/umbrella/ui/main/MainActivity.java
@@ -1,13 +1,48 @@
 package com.foo.umbrella.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.foo.umbrella.R;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.foo.umbrella.R;
+import com.foo.umbrella.ui.code.CodeActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  EditText editText;
+  TextView textView;
+  Button button;
+
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    editText = (EditText) findViewById(R.id.ed_id);
+    textView = (TextView) findViewById(R.id.tv_id);
+    button = (Button) findViewById(R.id.btn_id);
+
+    button.setOnClickListener(this);
+
+  }
+
+  @Override
+  public void onClick(View v) {
+
+     if(!editText.getText().toString().isEmpty()||editText != null ){
+
+       Intent intent = new Intent(this, CodeActivity.class);
+       intent.putExtra("zip",editText.getText().toString());
+       startActivity(intent);
+     }
+     else{
+       Toast.makeText(this,"Enter zipcode",Toast.LENGTH_LONG).show();
+     }
+
   }
 }
